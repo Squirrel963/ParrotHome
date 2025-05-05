813c7d27498d54ed72d3b526d7f165f638c93f5e
Squirrel963 <111523440+Squirrel963@users.noreply.github.com>
2025-05-05 10:25:15 +0800

    Add files via upload

--- pages/mc_price.py
+++ pages/mc_price.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_extras.metric_cards import style_metric_cards
 import requests
 import json
 import numpy as np
@@ -9,7 +10,7 @@ def get_data_from_api(api_url):
     data = response.content
     return data
 
-st.title("MineCraft正版价格监控")
+st.title("MineCraft正版价格监测")
 st.sidebar.page_link("PWUI.py", label="返回主页", icon=":material/home:",use_container_width=True)
 
 if st.button("获取数据",use_container_width=True):
@@ -25,11 +26,25 @@ if st.button("获取数据",use_container_width=True):
             Date.append(temp['date'])
             Deluxe.append(float(temp['Deluxe']))
             Standard.append(float(temp['Standard']))
+        st.write("完成！")
     col1, col2 = st.columns(2)
+    def card(text:str, value:float, type:str):
+        if type == "Deluxe":
+            delta = value - np.mean(Deluxe)
+        elif type == "Standard":
+            delta = value - np.mean(Standard)
+        
+        if delta != 0:
+            delta_color="inverse"
+        else:
+            delta_color="off"
+        st.metric(label=text, value=value, delta=delta,delta_color=delta_color)
+        #col3.metric(label="No Change", value=5000, delta=0)
+        style_metric_cards(background_color="#12161e",border_color="#6c86b6")
     with col1:
-        st.metric(label="标准版近期价格（RMB）", value=f"{Standard[-1]}", border=True, delta=f"{Standard[-2]-Standard[-1]}")
+        card(text="标准版近期价格（RMB）", value=Standard[-1], type="Standard")
     with col2:
-        st.metric(label="豪华版近期价格（RMB）", value=f"{Deluxe[-1]}", border=True, delta=f"{Deluxe[-2]-Deluxe[-1]}")
+        card(text="豪华版近期价格（RMB）", value=Deluxe[-1], type="Deluxe")
     data = {
         "date": Date,
         "豪华版本": Deluxe,
